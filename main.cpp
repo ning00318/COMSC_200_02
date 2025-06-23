5b6aceacb47ed755c5160d179d13373c164f433f
NINA <ninatsao@ningningning.lan>
2025-06-23 00:29:09 -0700

    4th commit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main()
     
     bool ifPlay = true;
     char yesOrNo;
+    int sumComputerPoints = 0;
+    int sumPlayerPoints = 0;
     while (ifPlay)
     {
         cout << "Would you like to roll the dice?" << endl;
@@ -20,15 +22,28 @@ int main()
         {
             Die die1(6);
             Die die2(6);
-            cout << "\n\nYou have " << die1.getValue() + die2.getValue() << " points." << endl << endl;
+            sumComputerPoints += die1.getValue();
+            sumPlayerPoints += die2.getValue();
+            cout << "\nYou have " << sumPlayerPoints << " points." << endl << endl;
         }
         else if (yesOrNo == 'N' || yesOrNo == 'n')
         {
             ifPlay = false;
+            cout << "\n--------------------------------" << endl;
         }
         else
-            cout << "Please enter the valid charracter";
+            cout << "Please enter the valid character";
     }
-    
+    cout << "The computer had " << sumComputerPoints << " points." << endl;
+    cout << "You had " << sumPlayerPoints << " points." << endl << endl;
 
+    if (sumPlayerPoints > sumComputerPoints)
+        cout << "Congratulations! You won!";
+    else if (sumPlayerPoints == sumComputerPoints)
+        cout << "Tie! No one loses or wins!";
+    else
+        cout << "Better luck next time.";
+    
+    cout << "\n\n--------------------------------\n\n";
+    cout << "Game Over\n\nPress any key to continue . . .\n\n";
 }
